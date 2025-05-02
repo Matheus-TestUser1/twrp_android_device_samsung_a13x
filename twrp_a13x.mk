9b33270db34451d9d120305519229078ae7a0b76
Matheus Alves <99508668+Matheus-TestUser1@users.noreply.github.com>
2025-05-02 15:09:38 -0300

    Update and rename twrp_a14xm.mk to twrp_a13x.mk

--- twrp_a13x.mk
+++ twrp_a13x.mk
@@ -13,17 +13,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from a14xm device
-$(call inherit-product, device/samsung/a14xm/device.mk)
+$(call inherit-product, device/samsung/a13x/device.mk)
 
-PRODUCT_DEVICE := a14xm
-PRODUCT_NAME := twrp_a14xm
+PRODUCT_DEVICE := a13x
+PRODUCT_NAME := twrp_a13x
 PRODUCT_BRAND := samsung
-PRODUCT_MODEL := Samsung Galaxy A14 5G
+PRODUCT_MODEL := SM-A136M
 PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="a14xmzh-user 13 TP1A.220624.014 A146PZHU2BWD1 release-keys"
-
-BUILD_FINGERPRINT := samsung/a14xmzh/a14xm:13/TP1A.220624.014/A146PZHU2BWD1:user/release-keys
